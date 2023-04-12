83ab865e4e26cdde6225e91fe591046a2c06cdbe
Dennis-Bersenev <dennis_bersenev@live.com>
2023-04-12 00:25:31 -0700

    improved example workbook and util bugfixes

--- src/utils.py
+++ src/utils.py
@@ -124,16 +124,17 @@ def directory_to_adata(data_dir: str, output_dir: str):
 """
 Convert and save the given object as a bytestream for quick future reloads. 
 """
-def write_pickle(object, save_path: str):
+def save_pickle(object, save_path: str):
     with open(save_path, 'wb') as outstream:
         pkl.dump(object, outstream)
 
 """
 Load in a pickled object. 
 """
-def load_pickle(object_name, save_path: str):
+def load_pickle(save_path: str):
     with open(save_path, 'rb') as instream:
         object_name = pkl.load(instream)
+    return object_name
 
 
 """
